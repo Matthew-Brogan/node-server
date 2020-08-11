dc422b88c93fdb907282314a895ba1399036a8c3
Matt Brogan <broganmatt11@gmail.com>
2020-08-11 13:02:45 -0400

    added all functionality

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static('public'));
 app.use((req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
